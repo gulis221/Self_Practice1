13f58d14fad4d9fa97b4d20658ce40ca657ee30e
gulis221 <gulis221@gmail.com>
2020-11-12 10:57:01 -0500

    practice pushing

---
+++ src/LoopPractice/ForLoop_.java
@@ -0,0 +1,19 @@
+package LoopPractice;
+
+public class ForLoop_ {
+    public static void main(String[] args) {
+
+        String []fruits = new String[5];
+        fruits [0] = "a";
+        fruits [1] = "b";
+        fruits [2] = "c";
+        fruits [3] = "d";
+        fruits [4] = "e";
+
+        for (int i =0;i <=5;i++) {
+            System.out.println(fruits[i]);
+        }
+
+
+    }
+}
